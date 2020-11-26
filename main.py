e83a91fca55c81a94864ef5894060d53f1385159
Marcin Klisz <marcinklisz@MacBook-Pro-Marcin.local>
2020-11-26 06:30:51 +0100

    add core functionality

--- main.py
+++ main.py
@@ -7,7 +7,6 @@ extensions = ('png','jpg','bmp','gif','ico')
 
 def check_dir(fileName):
     if os.path.exists(fileName):
-        print(os.getcwd())
         return True
     else:
         return False
@@ -34,4 +33,19 @@ parser.add_argument('-f','--format', required=True, choices=extensions, help='Im
 parser.add_argument('-r','--resize', type=float, help='Resizes Image with given ratio')
 args = parser.parse_args()
 
-print(get_imgdir(args.fname))
+iName = get_imgdir(args.fname)
+image = Image.open(get_imgdir(iName))
+if image.format != 'RGB' and args.format == 'jpg':
+    image = image.convert('RGB')
+if args.resize:
+    dim = (int(image.size[0]*args.resize), int(image.size[1]*args.resize))
+    image = image.resize(dim)
+    image.show()
+if args.outName == False:
+    iName = iName.split(".")
+    outName = iName[0] + "." +args.format
+    image.save(outName)
+else:
+    outName = args.outName.split(".")
+    outName = outName[0] +"."+ args.format
+    image.save(outName)    
